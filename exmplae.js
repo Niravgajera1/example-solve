bac789bbb21a3b7582ba39c19aaf646b2faad8f8
NiravGajera1 <niravgajera502@gmail.com>
2024-01-23 16:02:30 +0530

    switch sort add

--- exmplae.js
+++ exmplae.js
@@ -500,5 +500,26 @@
 //     return right;
 // }
 
+//-----------------------------------------------------------------------//
+
+// function SwitchSort(arr) {
+//     let swap = 0;
+//     let visited = new Array(arr.length).fill(false);
+//     for (let i = 0; i < arr.length; i++) {
+//       let j = i;
+//       let counter = 0;
+//       while (!visited[j]) {
+//         visited[j] = true;
+//         j = arr[j] - 1;
+//         counter = counter + 1;
+//       }
+//       if (counter !== 0) {
+//         swap = swap + (counter - 1);
+//       }
+//     }
+//     return swap;
+//   }
+  
+  
 
 
